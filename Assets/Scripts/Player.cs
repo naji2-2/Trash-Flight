d26b84c533c26431e96e7ec0a71a047449e01c29
Najisu <s2322@e-mirim.hs.kr>
2025-04-28 23:20:33 +0900

    플레이어와 코인의 충돌 처리

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,5 +45,10 @@ public class Player : MonoBehaviour
             Debug.Log("Game Over");
             Destroy(gameObject);
         }
+        else if (other.gameObject.tag == "Coin")
+        {
+            Debug.Log("Coin +1");
+            Destroy(other.gameObject);
+        }
     }
 }
