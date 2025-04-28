f3559b7f0531b4de13822effcf2e8b4c65457e1e
Najisu <s2322@e-mirim.hs.kr>
2025-04-28 15:01:51 +0900

    랜덤으로 적 만들기

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] enemies;
+
+    private float[] arrPosX = { -2.2f, -1.1f, 0f, 1.1f, 2.2f };
+
+    void Start()
+    {
+        foreach (float posX in arrPosX)
+        {
+            int index = Random.Range(0, enemies.Length);
+            SpawnEnemy(posX, index);
+        }
+        
+    }
+
+    void SpawnEnemy(float posX, int index)
+    {
+        Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
+        Instantiate(enemies[index], spawnPos, Quaternion.identity);
+    }
+
+}
